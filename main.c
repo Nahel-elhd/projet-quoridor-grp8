5617042bf226add6807635a33b59188b2e31aa8a
Nahel-elhd <elhaddaoui.nahel@gmail.com>
2024-11-13 12:38:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include"fct.h"
+
+int main()
+{
+    int choix;
+    afficherMenu();
+    scanf("%d", &choix);
+
+    switch (choix) {
+    case 1:
+        lancerNouvellePartie();
+        pion();
+
+        break;
+    case 2:
+        reprendrePartieSauvegardee();
+        break;
+    case 3:
+        afficherScores();
+
+        break;
+    case 4:
+        printf("Merci d'avoir joue ! A bientot.\n");
+        return 0;
+    default:
+        printf("Choix invalide. Veuillez reessayer.\n");
+        break;
+    }
+
+
+    return 0;
+}
 
