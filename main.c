f8ee1d741c0ba2d578ef49f16d1a3b9b2c4d5592
Nahel-elhd <elhaddaoui.nahel@gmail.com>
2024-11-23 14:40:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,45 +4,7 @@
 
 int main()
 {
-    int choix,nbr;
-    char plateau_de_jeu[TAILLE][TAILLE];
-    Joueur joueurs[4];
-    afficherMenu();
-    scanf("%d", &choix);
-
-    switch (choix) {
-    case 1:
-        lancerNouvellePartie();
-        CréationDuPlateau(plateau_de_jeu);
-        printf("choisissez le nombre de joueur  (2 ou 4 max) :\n");
-        scanf("%d",&nbr);
-        while (nbr!= 4 && nbr!= 2)
-        {
-            printf("ERREUR,veuillez choisis une valeur valide.\n");
-            scanf("%d",&nbr);
-        }
-        pion(joueurs,nbr, plateau_de_jeu);
-        Plateau(plateau_de_jeu);
-for(int tour=0; tour<5; tour++)
-{
-    deplacementDuPion(plateau_de_jeu, nbr, joueurs);
-    Plateau(plateau_de_jeu);
-}
-        break;
-    case 2:
-        reprendrePartieSauvegardee();
-        break;
-    case 3:
-        afficherScores();
-
-        break;
-    case 4:
-        printf("Merci d'avoir joue ! A bientot.\n");
-        return 0;
-    default:
-        printf("Choix invalide. Veuillez reessayer.\n");
-        break;
-    }
+gererjeu();
 
 
     return 0;
